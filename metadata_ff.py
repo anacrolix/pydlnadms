3899d1f8177b02d4ded045ca28f800799ed815e9
Matt Joiner <anacrolix@gmail.com>
2011-03-13 15:41:26 +1100

    Handle ffprobe and other ENOENT errors gracefully

--- metadata_ff.py
+++ metadata_ff.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python3
 
 def res_data(path):
+    from logging import getLogger
+    logger = getLogger()
     from subprocess import Popen, PIPE, CalledProcessError, list2cmdline
     def preexec_fn():
         from resource import setrlimit, RLIMIT_CPU
         # give the process 1 second, 2 if it treats SIGXCPU
         setrlimit(RLIMIT_CPU, (1, 2))
     args = ['ffprobe', '-show_format', path]
-    p = Popen(
-        args,
-        stdout=PIPE,
-        stderr=PIPE,
-        preexec_fn=preexec_fn,
-        close_fds=True)
+    try:
+        p = Popen(
+            args,
+            stdout=PIPE,
+            stderr=PIPE,
+            preexec_fn=preexec_fn,
+            close_fds=True
+        )
+    except OSError as exc:
+        if exc.errno == ENOENT:
+            logging.error('Failed to open %r: %s', list2cmdline(args), exc)
+            return {}
+        else:
+            raise
     stdout, stderr = p.communicate()
-    from logging import getLogger
-    logger = getLogger()
     if p.wait() != 0:
         logger.warning('{!r} failed with exit code {:d}'.format(
             list2cmdline(args),
